037b58986e4159c5e6b8a9228ed1eea080424808
gardusig <gustavo.gardusi@gmail.com>
2024-09-24 12:52:25 -0300

    Update paragraph format

--- src/main.ts
+++ src/main.ts
@@ -29,16 +29,18 @@ export function appendParagraphToFile(
   const file = GoogleDrive.findFile(folderPath, fileName);
   const doc = DocumentApp.openById(file.getId());
   const body = doc.getBody();
-  const paragraph =
-    body.getNumChildren() === 0
-      ? body.insertParagraph(0, content)
-      : body.appendParagraph(content);
-  GoogleDocs.formatParagraph(paragraph);
+  let paragraph: GoogleAppsScript.Document.Paragraph;
+  if (body.getNumChildren() === 0) {
+    paragraph = body.insertParagraph(0, content);
+  } else {
+    paragraph = body.appendParagraph(content);
+  }
   if (heading !== undefined) {
-    paragraph.setHeading(heading);
+    paragraph = paragraph.setHeading(heading);
     const emptyParagraph = body.appendParagraph("");
     GoogleDocs.formatParagraph(emptyParagraph);
   }
+  GoogleDocs.formatParagraph(paragraph);
   doc.saveAndClose();
   Logger.log(
     `Content appended to document "${fileName}" in folder "${folderPath}"`,
